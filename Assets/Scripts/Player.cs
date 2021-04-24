00d27c8aecc1731461f072ed2f7bfad7d508ac76
Deseteral <deseteral@gmail.com>
2021-04-24 09:25:43 +0200

    Simple tile map and player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float Speed = 3f;
+
+    private Rigidbody2D rigidbody;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        Vector2 delta = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) delta.y += Speed;
+        if (Input.GetKey(KeyCode.S)) delta.y -= Speed;
+        if (Input.GetKey(KeyCode.D)) delta.x += Speed;
+        if (Input.GetKey(KeyCode.A)) delta.x -= Speed;
+        
+        rigidbody.AddForce(delta, ForceMode2D.Force);
+    }
+}
