b4c19ef4b3d1ecaed8603733b07c76f752f94f5f
Deseteral <deseteral@gmail.com>
2021-04-25 03:59:25 +0200

    Fixed bug with cable center

--- Assets/Scripts/CableTile.cs
+++ Assets/Scripts/CableTile.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 public class CableTile : MonoBehaviour {
     public bool hasPower = false;
 
-    public SpriteRenderer center;
     public SpriteRenderer[] directionalSprites;
 
     private readonly Color DISABLED_COLOR = new Color(0x5A / 255f, 0x5A / 255f, 0x5A / 255f);
 
     public void ResetSprites(List<bool> neighbours) {
         // Reset values
-        center.enabled = false;
         foreach (var sprite in directionalSprites) {
             sprite.enabled = false;
         }
 
         // Enable sprites
+        bool anySet = false;
         for (int index = 0; index < neighbours.Count; index++) {
             directionalSprites[index].enabled = neighbours[index];
+            if (neighbours[index] == true) anySet = true;
         }
+
+        directionalSprites[4].enabled = !anySet; // Set center
         
         // Change color based on power status
         if (hasPower) {
-            center.color = Color.white;
             foreach (var sprite in directionalSprites) {
                 sprite.color = Color.white;
             }
         } else {
-            center.color = DISABLED_COLOR;
             foreach (var sprite in directionalSprites) {
                 sprite.color = DISABLED_COLOR;
             }
